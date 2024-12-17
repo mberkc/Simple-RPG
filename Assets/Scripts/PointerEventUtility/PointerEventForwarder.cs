a99855a8209418d56f2bdc336e0d895f87659ebc
Berk Çukacı <mberkcukaci@gmail.com>
2024-12-18 00:26:50 +0300

    feat(pointer event utility): forwards pointer events to parent

---
+++ Assets/Scripts/PointerEventUtility/PointerEventForwarder.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace PointerEventUtility
+{
+    public class PointerEventForwarder : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+    {
+        private PointerEventForwarderTarget pointerEventForwarderTarget;
+
+        private void Awake()
+        {
+            if (transform.parent == null)
+            {
+                Debug.LogWarning("PointerEventUtility: No Parent found!");
+                return;
+            }
+
+            pointerEventForwarderTarget = transform.parent.GetComponent<PointerEventForwarderTarget>();
+            
+            if (pointerEventForwarderTarget == null)
+                Debug.LogWarning("PointerEventUtility: Parent doesn't have PointerEventForwarderTarget component!");
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            pointerEventForwarderTarget?.OnPointerDown(eventData);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            pointerEventForwarderTarget?.OnPointerUp(eventData);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            pointerEventForwarderTarget?.OnPointerExit(eventData);
+        }
+    }
+}
---
+++ Assets/Scripts/PointerEventUtility/PointerEventForwarderTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace PointerEventUtility
+{
+    public abstract class PointerEventForwarderTarget: MonoBehaviour
+    {
+        public abstract void OnPointerDown(PointerEventData eventData);
+        public abstract void OnPointerUp(PointerEventData eventData);
+        public abstract void OnPointerExit(PointerEventData eventData);
+
+        public virtual void Awake()
+        {
+            if (transform.GetComponentInChildren<PointerEventForwarder>() == null)
+                Debug.LogWarning("PointerEventUtility: Don't have a PointerEventForwarder component in children.");
+        }
+    }
+}
