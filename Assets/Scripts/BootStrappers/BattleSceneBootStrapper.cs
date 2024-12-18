7740a5d11d63e6acd56edab36581bfbd55c07f83
Berk Çukacı <mberkcukaci@gmail.com>
2024-12-18 18:53:12 +0300

    feat(scene bootstrappers): add scenebootstrappers for each scene and assign canvas prefabs

---
+++ Assets/Scripts/BootStrappers/BattleSceneBootStrapper.cs
@@ -0,0 +1,28 @@
+﻿using Core.BootStrapper;
+using UnityEngine;
+
+namespace BootStrappers
+{
+    public class BattleSceneBootStrapper: SceneBootStrapper
+    {
+        [SerializeField] private GameObject battleCanvasPrefab;
+
+        public override void Initialize()
+        {
+            if (battleCanvasPrefab != null)
+            {
+                Instantiate(battleCanvasPrefab, transform);
+            }
+        }
+        
+        protected override void Start()
+        {
+            // Start Behavior
+        }
+
+        public override void OnDestroy()
+        {
+            // Cleanup Behavior
+        }
+    }
+}
---
+++ Assets/Scripts/BootStrappers/HeroSelectionSceneBootStrapper.cs
@@ -0,0 +1,28 @@
+﻿using Core.BootStrapper;
+using UnityEngine;
+
+namespace BootStrappers
+{
+    public class HeroSelectionSceneBootStrapper: SceneBootStrapper
+    {
+        [SerializeField] private GameObject heroSelectionCanvasPrefab;
+
+        public override void Initialize()
+        {
+            if (heroSelectionCanvasPrefab != null)
+            {
+                Instantiate(heroSelectionCanvasPrefab, transform);
+            }
+        }
+        
+        protected override void Start()
+        {
+            // Start Behavior
+        }
+
+        public override void OnDestroy()
+        {
+            // Cleanup Behavior
+        }
+    }
+}
