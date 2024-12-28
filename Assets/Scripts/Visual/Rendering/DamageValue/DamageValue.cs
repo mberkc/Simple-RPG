17791518bf0095fc03dec1eeb5e8788c073342a0
Berk Çukacı <mberkcukaci@gmail.com>
2024-12-28 23:29:07 +0300

    feat: add dotween sequence for damage value

--- Assets/Scripts/Visual/Rendering/DamageValue/DamageValue.cs
+++ Assets/Scripts/Visual/Rendering/DamageValue/DamageValue.cs
@@ -9,27 +9,31 @@ namespace Visual.Rendering.DamageValue
     {
         [SerializeField] private RectTransform rectTransform;
         [SerializeField] private TextMeshProUGUI textMeshPro;
-        private float verticalMove = Screen.height / 3f;
+        private float verticalMoveVector = Screen.height / 3f;
+        private Sequence sequence;
+        private Color startColor;
 
         internal void Show(float damage, Vector3 scenePosition, Action<DamageValue> recycle)
         {
             rectTransform.position = scenePosition;
             textMeshPro.text = damage.ToString("F0");
+            startColor = textMeshPro.color;
             
-            var startColor = textMeshPro.color;
-            textMeshPro.DOFade(0, 1.5f).SetEase(Ease.OutQuad).OnComplete(() =>
-            {
-                textMeshPro.color = startColor;
-                recycle?.Invoke(this);
-            });
+            sequence = DOTween.Sequence();
+            sequence.Append(textMeshPro.DOFade(0, 5f))
+                .Join(rectTransform.DOMoveY(rectTransform.position.y + verticalMoveVector, 5f))
+                .SetEase(Ease.OutQuad).OnComplete(() => Recycle(recycle));
+        }
 
-            rectTransform.DOMoveY(rectTransform.position.y + verticalMove, 1.5f).SetEase(Ease.OutQuad);
+        private void Recycle(Action<DamageValue> recycle)
+        {
+            textMeshPro.color = startColor;
+            recycle?.Invoke(this);
         }
         
         private void OnDestroy()
         {
-            // If it's animation while scene destroy => kill tween.
-            rectTransform.DOKill();
+            sequence?.Kill();
         }
     }
 }
